7a45e3be73f7119fb63a9dd5d0e8adb2488e6059
Christoph Gruener <christophgruener@me.com>
2019-03-22 14:49:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,6 +36,15 @@ class Student
   end
 
   def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql).collect do |student|
+      Student.new_from_db(student)
+    end
+      
   end
 
   def save
